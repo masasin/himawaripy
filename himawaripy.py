936f1853fedab456e0ef16bbcb7b159295d33f7f
masasin <mshtsy+github@gmail.com>
2016-03-14 04:23:08 +0900

    Add capitalization.

--- himawaripy.py
+++ himawaripy.py
@@ -16,40 +16,40 @@ from tqdm import tqdm
 from utils import get_desktop_environment
 
 
-_home_dir = os.getenv("USER_HOME", "/tmp")
-_log_dir = os.path.join(_home_dir, ".logs")
-_log_file = os.path.join(_log_dir, "himawaripy.log")
-os.makedirs(_log_dir, exist_ok=True)
+_HOME_DIR = os.getenv("USER_HOME", "/tmp")
+_LOG_DIR = os.path.join(_HOME_DIR, ".logs")
+_LOG_FILE = os.path.join(_LOG_DIR, "himawaripy.log")
+os.makedirs(_LOG_DIR, exist_ok=True)
 
-json_url = "http://himawari8-dl.nict.go.jp/himawari8/img/D531106/latest.json"
-image_url = "http://himawari8.nict.go.jp/img/D531106/{}d/{}/{}_{}_{}.png"
-timeout = 1  # second
+JSON_URL = "http://himawari8-dl.nict.go.jp/himawari8/img/D531106/latest.json"
+IMAGE_URL = "http://himawari8.nict.go.jp/img/D531106/{}d/{}/{}_{}_{}.png"
+TIMEOUT = 1  # second
 
 # Time formats
-logfile_fmt = "%(name)-12s : %(levelname)-8s  %(message)s"
-console_fmt = "%(levelname)-8s : %(message)s"
-date_fmt_iso = "%Y-%m-%d %H:%M:%S"
-date_fmt_url = "%Y/%m/%d/%H%M%S"
+LOGFILE_FMT = "%(name)-12s : %(levelname)-8s  %(message)s"
+CONSOLE_FMT = "%(levelname)-8s : %(message)s"
+DATE_FMT_ISO = "%Y-%m-%d %H:%M:%S"
+DATE_FMT_URL = "%Y/%m/%d/%H%M%S"
 
 # Tile size
-width = 550
-height = 550
+WIDTH = 550
+HEIGHT = 550
 
-level = 4  # Increases the quality and the size. Possible values: 4, 8, 16, 20
+LEVEL = 4  # Increases the quality and the size. Possible values: 4, 8, 16, 20
 
-output_file = os.path.expanduser("~/.config/himawari/himawari-latest.png")
+OUTPUT_FILE = os.path.expanduser("~/.config/himawari/himawari-latest.png")
 
 # Log everything to file
 logging.basicConfig(level=logging.DEBUG,
-                    format="%(asctime)s " + logfile_fmt,
-                    datefmt=date_fmt_iso,
-                    filename=_log_file,
+                    format="%(asctime)s " + LOGFILE_FMT,
+                    datefmt=DATE_FMT_ISO,
+                    filename=_LOG_FILE,
                     filemode="a")
 
 # Log important data to console
 console = logging.StreamHandler()
 console.setLevel(logging.INFO)
-console.setFormatter(logging.Formatter(console_fmt))
+console.setFormatter(logging.Formatter(CONSOLE_FMT))
 logging.getLogger("").addHandler(console)
 
 # Hide info logs from requests
@@ -61,10 +61,10 @@ logger = logging.getLogger("himawaripy")
 
 def main():
     logger.info("Updating...")
-    latest_timestamp = get_latest_timestamp(json_url)
+    latest_timestamp = get_latest_timestamp(JSON_URL)
     logger.info("Latest version: {} GMT"
-                .format(strftime(date_fmt_iso, latest_timestamp)))
-    time_as_url = strftime(date_fmt_url, latest_timestamp)
+                .format(strftime(DATE_FMT_ISO, latest_timestamp)))
+    time_as_url = strftime(DATE_FMT_URL, latest_timestamp)
 
     with create_png() as image:
         build_png(image, time_as_url)
@@ -73,29 +73,29 @@ def main():
     logger.info("Done!")
 
 
-def get_latest_timestamp(json_url):
-    latest_json = requests.get(json_url, timeout=timeout)
-    return strptime(loads(latest_json.content.decode())["date"], date_fmt_iso)
+def get_latest_timestamp(JSON_URL):
+    latest_json = requests.get(JSON_URL, timeout=TIMEOUT)
+    return strptime(loads(latest_json.content.decode())["date"], DATE_FMT_ISO)
 
 
 @contextmanager
 def create_png():
-    png = Image.new('RGB', (width*level, height*level))
+    png = Image.new('RGB', (WIDTH*LEVEL, HEIGHT*LEVEL))
     yield png
     logger.info("Saving image")
-    os.makedirs(os.path.split(output_file)[0], exist_ok=True)
-    png.save(output_file, "PNG")
+    os.makedirs(os.path.split(OUTPUT_FILE)[0], exist_ok=True)
+    png.save(OUTPUT_FILE, "PNG")
 
 
 def build_png(png, time_as_url):
-    with tqdm(desc="Tiles downloaded", total=level**2, leave=True,
+    with tqdm(desc="Tiles downloaded", total=LEVEL**2, leave=True,
               unit="tile", unit_scale="true") as pbar:
-        for x in range(level):
-            for y in range(level):
-                tile_url = image_url.format(level, width, time_as_url, x, y)
-                tiledata = requests.get(tile_url, timeout=timeout).content
+        for x in range(LEVEL):
+            for y in range(LEVEL):
+                tile_url = IMAGE_URL.format(LEVEL, WIDTH, time_as_url, x, y)
+                tiledata = requests.get(tile_url, timeout=TIMEOUT).content
                 tile = Image.open(BytesIO(tiledata))
-                png.paste(tile, (width*x, height*y, width*(x+1), height*(y+1)))
+                png.paste(tile, (WIDTH*x, HEIGHT*y, WIDTH*(x+1), HEIGHT*(y+1)))
                 pbar.update()
 
 
@@ -109,23 +109,23 @@ def set_background():
             call("gsettings set org.gnome.desktop.background "
                  "draw-background false".split())
         call("gsettings set org.gnome.desktop.background picture-uri "
-             "file://{}".format(output_file).split())
+             "file://{}".format(OUTPUT_FILE).split())
         call("gsettings set org.gnome.desktop.background "
              "picture-options scaled".split())
     elif de == "mate":
         call('gconftool-2 -type string -set '
              '/desktop/gnome/background/picture_filename "{}"'
-             .format(output_file).split())
+             .format(OUTPUT_FILE).split())
     elif de == "xfce4":
         call("xfconf-query --channel xfce4-desktop --property "
                 "/backdrop/screen0/monitor0/image-path --set {}"
-             .format(output_file).split())
+             .format(OUTPUT_FILE).split())
     elif de == "lxde":
-        call("display -window root {}".format(output_file).split())
+        call("display -window root {}".format(OUTPUT_FILE).split())
     elif de == "mac":
         call('osascript -e \'tell application "Finder" to set '
              'desktop picture to POSIX file "{}"\''
-             .format(output_file).split())
+             .format(OUTPUT_FILE).split())
     else:
         logger.error("Your desktop environment '{}' is not supported"
                      .format(de))
